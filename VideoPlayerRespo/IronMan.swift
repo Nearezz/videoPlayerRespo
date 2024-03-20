03022c3856c94882eed7be8b3b7daeea121b64f1
Nearezz <biswasindranil019>
2024-03-20 10:00:05 -0400

    Done with video

---
+++ VideoPlayerRespo/IronMan.swift
@@ -0,0 +1,18 @@
+//
+//  IronMan.swift
+//  VideoPlayerRespo
+//
+//  Created by NYCDOE on 3/20/24.
+//
+
+import SwiftUI
+
+struct IronMan: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IronMan()
+}
---
+++ VideoPlayerRespo/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  VideoPlayerRespo
+//
+//  Created by NYCDOE on 3/20/24.
+//
+
+import SwiftUI
+
+struct Video: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Video()
+}
