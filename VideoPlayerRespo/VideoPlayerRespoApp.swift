abb01ec52975dd115264502c57f2ec349a52997e
Nearezz <biswasindranil019>
2024-03-20 09:38:18 -0400

    Initial Commit

---
+++ VideoPlayerRespo/VideoPlayerRespoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerRespoApp.swift
+//  VideoPlayerRespo
+//
+//  Created by NYCDOE on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerRespoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
